06cd41dd102d239c80dc9395b5d1785bca9d67cb
Ignacio Bonilla Rojas <129819050+NachoBonilla03@users.noreply.github.com>
2024-06-11 17:43:44 -0600

    Update app.py
    Cambio en app para que use flask

--- app/app.py
+++ app/app.py
@@ -1,27 +1,58 @@
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
 def dibujar_cuadrado(tamano):
+    square = ""
     for i in range(tamano):
         if i == 0 or i == tamano - 1:
-            print("*" * tamano)
+            square += "*" * tamano + "\n"
         else:
-            print("*" + " " * (tamano - 2) + "*")
+            square += "*" + " " * (tamano - 2) + "*" + "\n"
+    return square
 
 def dibujar_triangulo(tamano):
+    triangle = ""
     for i in range(tamano):
-        print(" " * (tamano - i - 1) + "*" * (2 * i + 1))
+        triangle += " " * (tamano - i - 1) + "*" * (2 * i + 1) + "\n"
+    return triangle
 
 def dibujar_circulo(tamano):
+    circle = ""
     for i in range(tamano):
         for j in range(tamano):
             if (i - tamano // 2) ** 2 + (j - tamano // 2) ** 2 <= (tamano // 2) ** 2:
-                print("*", end="")
+                circle += "*"
             else:
-                print(" ", end="")
-        print()
+                circle += " "
+        circle += "\n"
+    return circle
 
-# Dibujar figuras
-print("Cuadrado:")
-dibujar_cuadrado(5)
-print("\nTriángulo:")
-dibujar_triangulo(5)
-print("\nCírculo:")
-dibujar_circulo(10)
+@app.route('/')
+def home():
+    return '''
+    <h1>Choose a shape to draw</h1>
+    <ul>
+        <li><a href="/cuadrado/5">Draw 5x5 Square</a></li>
+        <li><a href="/triangulo/5">Draw 5-row Triangle</a></li>
+        <li><a href="/circulo/10">Draw 10x10 Circle</a></li>
+    </ul>
+    '''
+
+@app.route('/cuadrado/<int:tamano>')
+def cuadrado(tamano):
+    square = dibujar_cuadrado(tamano)
+    return render_template_string('<pre>{{ square }}</pre>', square=square)
+
+@app.route('/triangulo/<int:tamano>')
+def triangulo(tamano):
+    triangle = dibujar_triangulo(tamano)
+    return render_template_string('<pre>{{ triangle }}</pre>', triangle=triangle)
+
+@app.route('/circulo/<int:tamano>')
+def circulo(tamano):
+    circle = dibujar_circulo(tamano)
+    return render_template_string('<pre>{{ circle }}</pre>', circle=circle)
+
+if __name__ == '__main__':
+    app.run(debug=True)
